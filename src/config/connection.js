1b46b59cdfac691a1132c68c9dded6812788286f
Brendon Nogueira <brendonnogueira21@gmail.com>
2024-10-22 19:20:14 -0300

    modificando os arquivos de configurações e model

---
+++ src/config/connection.js
@@ -0,0 +1,8 @@
+module.exports = {
+    username: 'root',
+    password: 'root',
+    database: 'blog_tech',
+    host: 'localhost',
+    dialect: 'mysql',
+   
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const db = require('./model') 
 const router = require('./router/index.router')
-
 
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 // Middlewares
@@ -14,8 +16,21 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 
-
-
 app.use('/', router)
 
-app.listen(8088, () => console.log('Server is running on port 8088'))
+// Autentica a conexão com o banco de dados
+db.sequelize.authenticate()
+  .then(() => {
+    console.log('Conexão estabelecida com sucesso.')
+
+    
+    return db.sequelize.sync({ alter: true })
+  })
+  .then(() => {
+    console.log('Tabelas sincronizadas com sucesso.')
+
+    app.listen(8088, () => console.log('Server is running on port 8088'))
+  })
+  .catch(err => {
+    console.error('Erro ao conectar no banco de dados ou sincronizar tabelas:', err)
+  })
